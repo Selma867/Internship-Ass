397ed3711d55d6a9ea2586d4a69e53ffe21aa881
Selma <166963039+Selma867@users.noreply.github.com>
2025-10-10 23:37:03 +0200

    Add files via upload

---
+++ question 1.sql
@@ -0,0 +1,14 @@
+-- Database: registration_db
+
+-- DROP DATABASE IF EXISTS registration_db;
+
+CREATE DATABASE registration_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
